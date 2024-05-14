7063b8c98dd0b98a6d325f44e2a570eaac344125
Alexander <dantedmc78@gmail.com>
2024-05-14 18:55:41 +0300

    Update: ThirdUnitBrain - added state transitions & task#5

--- Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
+++ Assets/Scripts/UnitBrains/Player/ThirdUnitBrain.cs
@@ -15,40 +15,57 @@ namespace UnitBrains.Player
 
     public class ThirdUnitBrain : DefaultPlayerUnitBrain {
         public override string TargetUnitName => "Ironclad Behemoth";
+        // public static int UnitCounter = 0;
+        // private int UnitID = UnitCounter++;
         private float StateSwitchCooldownTimeSeconds = 1f;
         private float _stateSwitchCooldownTime = 0f;
         private UnitState _prevUnitState = UnitState.Move;
         private UnitState _unitState = UnitState.Move;
 
+        private void ToggleState(UnitState state) {
+            if (state == _unitState) {
+                return;
+            }
+            // if (state != UnitState.Switch) {
+            //     Debug.Log($"Unit#{UnitID} toggling to State \"{state}\"");
+            // }
+            _prevUnitState = _unitState;
+            _unitState = state;
+        }
+
         public override void Update(float deltaTime, float time)
         {
             base.Update(deltaTime, time);
-
             if (_unitState == UnitState.Switch) {
                 _stateSwitchCooldownTime += Time.deltaTime;
-                float t = _stateSwitchCooldownTime / (StateSwitchCooldownTimeSeconds/10);
-                if (t >= 1) {
+                if (_stateSwitchCooldownTime >= StateSwitchCooldownTimeSeconds) {
                     _stateSwitchCooldownTime = 0;
-                    if (_prevUnitState == UnitState.Move) {
-                        _prevUnitState = _unitState;
-                        _unitState = UnitState.Attack;
-                    } else if (_prevUnitState == UnitState.Attack) {
-                        _prevUnitState = _unitState;
-                        _unitState = UnitState.Move;
-                    }
+                    ToggleState( (_prevUnitState == UnitState.Move) ? UnitState.Attack : UnitState.Move );
                 }
             }
         }
 
         protected override List<Vector2Int> SelectTargets()
         {
-            // TODO: block if switching action or attacking
-            return base.SelectTargets();
+            var targets = base.SelectTargets();
+            if (_unitState != UnitState.Attack) {
+                return new();
+            }
+            if (!targets.Any()) {
+                ToggleState(UnitState.Switch);
+            }
+            return targets;
         }
 
         public override Vector2Int GetNextStep() {
-            // TODO: block if switching action or moving
-            return base.GetNextStep();
+            var nextStep = base.GetNextStep();
+            if (_unitState != UnitState.Move) {
+                return unit.Pos;
+            }
+            if (nextStep == unit.Pos) {
+                ToggleState(UnitState.Switch);
+            }
+            return nextStep;
         }
     }
 }
