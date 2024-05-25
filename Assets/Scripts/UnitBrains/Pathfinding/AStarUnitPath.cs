8195e19cde3751f982966111e364a4851f8168c6
Alexander <dantedmc78@gmail.com>
2024-05-25 14:05:05 +0300

    refactor: AStarUnitPath - trying out another estimate calculation algorithm (based on Wikipedia article)

--- Assets/Scripts/UnitBrains/Pathfinding/AStarUnitPath.cs
+++ Assets/Scripts/UnitBrains/Pathfinding/AStarUnitPath.cs
@@ -1,21 +1,23 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Model;
 using UnityEngine;
-using Utilities;
 
 namespace UnitBrains.Pathfinding
 {
     public class AStarUnitPath : BaseUnitPath
     {
-        private Vector2Int[] dxy = {
+        private Vector2Int[] successors = {
             Vector2Int.down,
+            //new Vector2Int(-1, -1), //down-left
             Vector2Int.left,
+            //new Vector2Int(-1, 1), //up-left
+            Vector2Int.up,
+            //new Vector2Int(1, 1), //up-right
             Vector2Int.right,
-            Vector2Int.up
+            //new Vector2Int(1, -1), //down-right
         };
-        private const int MaxLength = 100;
+        //private const int MaxLength = 100;
         public AStarUnitPath(IReadOnlyRuntimeModel runtimeModel, Vector2Int startPoint, Vector2Int endPoint) : base(runtimeModel, startPoint, endPoint)
         {
         }
@@ -30,59 +32,106 @@ namespace UnitBrains.Pathfinding
             }
         }
 
-        private int CalculateEstimate(Vector2Int fromPos, Vector2Int toPos) => Math.Abs(fromPos.x - toPos.x) + Math.Abs(fromPos.y - toPos.y);
+        //private int CalculateEstimate(Vector2Int fromPos, Vector2Int toPos) => Math.Abs(fromPos.x - toPos.x) + Math.Abs(fromPos.y - toPos.y);
+        private int CalculateEstimate(Vector2Int fromPos, Vector2Int toPos) => (int)Math.Sqrt(
+                                    Math.Pow(toPos.x - fromPos.x, 2) +
+                                    Math.Pow(toPos.y - fromPos.y, 2)
+                                );
+
+        private List<Vector2Int> getPathFromNode(Node node)
+        {
+            List<Vector2Int> _path = new();
+            while (node != null)
+            {
+                _path.Add(node.Pos);
+                node = node.Parent;
+            }
+            return _path;
+        }
 
         private List<Vector2Int> CalculateAStar(Vector2Int fromPos, Vector2Int toPos)
         {
-            var counter = 0;
-            var routeFound = false;
             Node startNode = new(fromPos, CalculateEstimate(fromPos, toPos));
             List<Node> openList = new() { startNode };
             HashSet<Vector2Int> closedList = new();
-            Node currentNode = openList[0];
-            while (openList.Count > 0 && counter++ < MaxLength) {
-                foreach (var node in openList) {
-                    if (node.Value < currentNode.Value) {
+            bool routeFound = false;
+
+            while(openList.Count > 0)
+            {
+                Node currentNode = openList[0];
+                foreach (var node in openList)
+                {
+                    if (node.Value < currentNode.Value)
+                    {
                         currentNode = node;
                     }
                 }
-
                 openList.Remove(currentNode);
-                closedList.Add(currentNode.CoordinatePoint);
+                if (closedList.Contains(currentNode.Pos))
+                    continue;
 
-                if (routeFound) {
-                    List<Vector2Int> _path = new();
-                    while(currentNode != null) {
-                        _path.Add(currentNode.CoordinatePoint);
-                        currentNode = currentNode.Parent;
-                    }
-                    return _path;
+                if (routeFound)
+                {
+                    return getPathFromNode(currentNode);
                 }
 
-                for (int i = 0; i < dxy.Length; i++) {
-                    Vector2Int newPoint = currentNode.CoordinatePoint + dxy[i];
+                closedList.Add(currentNode.Pos);
+
+                foreach (Vector2Int n in successors)
+                {
+                    Vector2Int newPoint = currentNode.Pos + n;
+
                     if (closedList.Contains(newPoint))
                         continue;
 
-                    if (newPoint == toPos)
+                    if (newPoint == endPoint)
+                    {
                         routeFound = true;
-
-                    if (runtimeModel.IsTileWalkable(newPoint) || routeFound) {
-                        Node neighbor = new(newPoint, CalculateEstimate(newPoint, toPos), startNode.Cost, currentNode);
-                        bool hasOpenNode = false;
-                        foreach (var node in openList) {
-                            if (node.CoordinatePoint == newPoint) {
-                                hasOpenNode = true;
-                                break;
-                            }
-                        }
-                        if (!hasOpenNode)
-                            openList.Add(neighbor);
+                        break;
                     }
+
+                    if (!runtimeModel.IsTileWalkable(newPoint))
+                        continue;
+
+                    CalculateNeigborWeights(openList, currentNode, newPoint);
                 }
+
             }
             // TODO: handle border cases (when route is not found)
-            return new() { fromPos };
+            //return new() { fromPos };
+            return null;
+        }
+
+        private void CalculateNeigborWeights(List<Node> openList, Node currentNode, Vector2Int newPoint)
+        {
+            var new_g = currentNode.G + 1;
+
+            int nfo_i = -1; //if neighbor is in OpenList, set nfo_i to its index
+            for (var i = 0; i < openList.Count; i++)
+            {
+                if (openList[i].Pos == newPoint)
+                {
+                    nfo_i = i;
+                    break;
+                }
+            }
+            if (nfo_i >= 0) //if neighbor found
+            {
+                if (new_g < openList[nfo_i].G)
+                { //update and recalculate weights
+                    openList[nfo_i].G = new_g;
+                    openList[nfo_i].H = CalculateEstimate(openList[nfo_i].Pos, endPoint);
+                    openList[nfo_i].Parent = currentNode;
+                }
+            }
+            else //if neighbor is not in openList, just add it
+            {
+                openList.Add(new Node(
+                    newPoint,
+                    CalculateEstimate(newPoint, endPoint),
+                    new_g,
+                    currentNode));
+            }
         }
     }
 }
