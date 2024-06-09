c8dd634bc426cff05e879d93bf19f947526471de
Alexander <dantedmc78@gmail.com>
2024-06-09 11:27:02 +0300

    add: dummy Coordinator singleton

---
+++ Assets/Scripts/UnitBrains/Coordinator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assets.Scripts.UnitBrains
+{
+    public class Coordinator
+    {
+        private static Coordinator? instance;
+
+        private Coordinator() { }
+
+        public static Coordinator GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Coordinator();
+            }
+            return instance;
+        }
+    }
+}
