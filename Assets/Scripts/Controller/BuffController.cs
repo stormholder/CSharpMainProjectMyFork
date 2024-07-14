b5c7615e6e6031eb35ebf5d2457a392b093d192c
Alexander <dantedmc78@gmail.com>
2024-07-14 15:20:33 +0300

    update BuffController - add dictionary with buffs, add updateCoroutine

--- Assets/Scripts/Controller/BuffController.cs
+++ Assets/Scripts/Controller/BuffController.cs
@@ -1,13 +1,63 @@
-﻿using System;
+﻿using Model.Runtime;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnitBrains.Buff;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utilities;
 
 namespace Controller
 {
-    public class BuffController
+    public class BuffController : IDisposable
     {
+        Dictionary<Unit, List<BaseBuff>> _unitBuffs = new();
+        private TimeUtil _time = ServiceLocator.Get<TimeUtil>();
 
+        public BuffController()
+        {
+            _time.AddFixedUpdateAction(UpdateCoroutine);
+        }
+
+        public void AddUnitBuff(Unit unit, BaseBuff buff)
+        {
+            if (!_unitBuffs.ContainsKey(unit))
+            {
+                _unitBuffs.Add(unit, new List<BaseBuff>());
+            }
+            _unitBuffs[unit].Add(buff);
+        }
+
+        public List<BaseBuff> GetUnitBuffs(Unit unit)
+        {
+            return (_unitBuffs.ContainsKey(unit)) ? _unitBuffs[unit] : new();
+        }
+        private void UpdateCoroutine(float timeDelta)
+        {
+            if (_unitBuffs.Values.Count == 0)
+                return;
+            foreach (var unitBuffList in _unitBuffs.Values)
+            {
+                foreach (var unitBuff in unitBuffList)
+                {
+                    unitBuff.Duration -= timeDelta;
+                    if (unitBuff.Duration <= 0)
+                        unitBuffList.Remove(unitBuff);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            _time.RemoveFixedUpdateAction(UpdateCoroutine);
+            //if (_updateCoroutine != null)
+            //{
+            //    _time.StopCoroutine(_updateCoroutine);
+            //    _updateCoroutine = null;
+            //}
+        }
     }
 }
