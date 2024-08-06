ffdd28e8646cb0b537423b036a7dc08d38767da3
Alexander <dantedmc78@gmail.com>
2024-08-06 22:29:50 +0300

    refactor BaseBuff - added IBuffable interface, BaseBuff implements it

--- Assets/Scripts/UnitBrains/Buff/BaseBuff.cs
+++ Assets/Scripts/UnitBrains/Buff/BaseBuff.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Model.Runtime;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,16 +8,47 @@ using UnityEngine;
 
 namespace UnitBrains.Buff
 {
+    public interface IBuffable : IDisposable
+    {
+        public void Activate();
 
-    public class BaseBuff
+        public bool CanApplyToUnit();
+
+        public void ReduceDurationByDelta(float delta);
+        public bool IsActive();
+    }
+
+    public class BaseBuff : IBuffable
     {
         public BuffType BuffType = BuffType.None;
         public float Duration { get; set; }
-        public float Modifier { get; private set; }
+        protected float modifier;
+        protected Unit unit;
 
-        public BaseBuff(float duration, float modifier) {
-            Duration = duration;
-            Modifier = modifier;
+        public BaseBuff(Unit unit, float duration, float modifier) {
+            this.unit = unit;
+            this.Duration = duration;
+            this.modifier = modifier;
+        }
+
+        public virtual void Activate()
+        {
+        }
+
+        public virtual bool CanApplyToUnit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ReduceDurationByDelta(float delta)
+        {
+            Duration -= delta;
+        }
+
+        public bool IsActive() => Duration > 0;
+
+        public virtual void Dispose()
+        {
         }
     }
 }
